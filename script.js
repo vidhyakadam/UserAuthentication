aa8e7abf68d74a5ef96da381813810822fa3cb88
vidhyakadam <vidhyakadam124@gmail.com>
2023-11-26 10:12:21 +0530

    adding file

--- script.js
+++ script.js
@@ -1,41 +1,85 @@
+function generateRandomToken() {
+    return Math.floor(Math.random() * 900000 + 100000);
+}
 
-////-----------------------form validation code is here ------------------------------------------
-function validateform(){
-    var name =  document.myform.name.value;
-    var firstpassword = document.myform.password.value;
-    var secondpassword = document.myform.password2.value;
+// local storAge
 
-    if(name==null || name ==""){
-        alert("Name field cant be blank")
-        return false;
-    }
-      if(firstpassword == secondpassword){
-        return true;
-    }
-    else {
-        alert("password must be smae!");
-    }
+// saving DaTa
+function saveToLocalStorage(fullName, email, password, token) {
+    localStorage.setItem('fullName', fullName);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    localStorage.setItem('token', token);
+}
+// Getting Data fROm local storAge
+function getFromLocalStorage() {
+    const fullName = localStorage.getItem('fullName');
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+    const token = localStorage.getItem('token');
+    return { fullName, email, password, token };
 }
 
 
-//-----------------------main 
+// filling details 
+function showProfile() {
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
 
-document.addEventListener('DOMContentLoaded', () => {
+    if (!fullName || !email || !password || !confirmPassword) {
+        document.getElementById("error-message").textContent = "Oops! All fields are mandatory.";
+        return;
+    }
 
+    if (password !== confirmPassword) {
+        document.getElementById("error-message").textContent = "Oops! Passwords do not match.";
+        return;
+    } else {
+        document.getElementById("error-message").textContent = "";
+    }
 
+     // Checking if user is already logged in
+     const storedData = getFromLocalStorage();
+     let token;
+     if (storedData.email) {
+         //if already logged in, reuse the existing token
+         token = storedData.token;
+     } else {
+         // ifnot logged in, generate a new token
+         token = generateRandomToken();
+     }
+    saveToLocalStorage(fullName, email, password, token);
 
-});
+    document.getElementById("profileFullName").textContent = " " + fullName;
+    document.getElementById("profileEmail").textContent = " " + email;
+    document.getElementById("profileToken").textContent = " " + token;
+    document.getElementById("profilePassword").textContent = " " + password;
 
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("dashboard").style.display = "block";
+    document.getElementById("profile").style.display = "flex";
+}
 
+function showSignupForm() {
+    localStorage.clear();
+    document.getElementById("signup-form").style.display = "block";
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("dashboard").style.display = "none";
 
+    document.getElementById("signup").reset();
+    document.getElementById("error-message").textContent = "";
+}
 
-// function saveData(){
-//     let name, email, password;
-//     name = document.getElementById("#name").value;
-//     email = document.getElementById("#email").value;
-//     password = document.getElementById("#password").value;
-
-//     localStorage.setItem("name", name);
-//     localStorage.setItem("email", email);
-//     localStorage.setItem("password", password);
-// }
+//data not lost even after reloading the pAge
+window.onload = function() {
+    const storedData = getFromLocalStorage();
+    if (storedData.email) {
+        document.getElementById("fullName").value = storedData.fullName;
+        document.getElementById("email").value = storedData.email;
+        document.getElementById("password").value = storedData.password;
+        document.getElementById("confirmPassword").value = storedData.password;
+        showProfile();
+    }
+}
